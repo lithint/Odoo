e256c9595d3ecc2089a3684ba13b8795ed479aac
GentecAustralia <46512820+GentecAustralia@users.noreply.github.com>
2019-02-22 07:21:16 +1100

    Update custom_stock_picking.py

--- domain366/custom_au_in/models/custom_stock_picking.py
+++ domain366/custom_au_in/models/custom_stock_picking.py
@@ -10,28 +10,29 @@ class StockPicking(models.Model):
         sale_order  =  self.env['sale.order'].search([('name', '=',self.origin )])
         sale_order_line_obj = self.env['sale.order.line']
         inv_line = False
-        for pick_line in self.move_lines:
-            account = self.get_account_properties()
-            inv_line = invoice_line_obj.create({
-                    'name': pick_line.name,
-                    'account_id': account.id,
-                    'invoice_id':inv_obj.id,
-                    'price_unit': pick_line.price_unit *-1,
-                    'quantity': pick_line.product_uom_qty,
-                    'uom_id': pick_line.product_id.uom_id.id,
-                    'product_id': pick_line.product_id.id,
-                    })
-            order_line_ids = sale_order_line_obj.search([('order_id', '=', sale_order.id),('product_id', '=',pick_line.product_id.id )])
-            for order_line in order_line_ids:
-                order_line .write({'qty_to_invoice':pick_line.product_uom_qty,'invoice_lines':[(4,inv_line.id)]})
+        for sale_line in sale_order.order_line:
+            if not sale_line.product_id.product_tmpl_id.bom_ids:
+                account = self.get_account_properties()
+                inv_line = invoice_line_obj.create({
+                        'name': sale_line.name,
+                        'account_id': account.id,
+                        'invoice_id':inv_obj.id,
+                        'price_unit': sale_line.price_unit,
+                        'quantity': sale_line.product_uom_qty,
+                        'uom_id': sale_line.product_id.uom_id.id,
+                        'product_id': sale_line.product_id.id,
+                        })
+                order_line_ids = sale_order_line_obj.search([('order_id', '=', sale_order.id),('product_id', '=',sale_line.product_id.id )])
+                for order_line in order_line_ids:
+                    order_line.write({'qty_to_invoice':sale_line.product_uom_qty,'invoice_lines':[(4,inv_line.id)]})
 
-            tax_ids = []
-            if order_line and order_line_ids[0]:
-                for tax in order_line[0].tax_id:
-                    tax_ids.append(tax.id)
+                    tax_ids = []
+                    if order_line and order_line_ids[0]:
+                        for tax in order_line[0].tax_id:
+                            tax_ids.append(tax.id)
 
-            inv_line.write({'price_unit':order_line[0].price_unit, 'discount': order_line[0].discount, 'invoice_line_tax_ids': [(6,0,tax_ids)]   })
-            inv_obj.compute_taxes()
+                    inv_line.write({'price_unit':order_line[0].price_unit, 'discount': order_line[0].discount, 'invoice_line_tax_ids': [(6,0,tax_ids)]   })
+                    inv_obj.compute_taxes()
         return True
 
     def get_account_properties(self):
@@ -44,7 +45,7 @@ class StockPicking(models.Model):
             elif pick_line.product_id.categ_id.property_account_income_categ_id:
                 account = pick_line.product_id.categ_id.property_account_income_categ_id
             else:
-                ir_property_obj.search([('name', '=', 'property_account_income_categ_id')])
+                account_search = ir_property_obj.search([('name', '=', 'property_account_income_categ_id')])
                 account = account_search.value_reference
                 account = account.split(",")[1]
                 account = account_obj.browse(account)
@@ -62,8 +63,7 @@ class StockPicking(models.Model):
                 'account_id': account.id,
                 'invoice_id': account_invoice_obj.id,
                 'price_unit': sale_line.price_unit,
-                # 'quantity': 0.0,
-                'quantity': sale_line.product_uom_qty,
+                'quantity': 0.0,
                 'uom_id': sale_line.product_id.uom_id.id,
                 'product_id': sale_line.product_id.id})
             sale_line .write({'qty_to_invoice':sale_line.qty_delivered})
@@ -93,8 +93,7 @@ class StockPicking(models.Model):
                 inv_lines.unlink()
             for sale_line in sale_order.order_line:
                 if sale_line.product_id.product_tmpl_id.bom_ids:
-                    # if inital_qty != ship_qty:
-                    if inital_qty == ship_qty:
+                    if inital_qty != ship_qty:
                         self.invoice_lines_creation()
                     else:
                         invoice_line_id = inv_line_obj.create({
