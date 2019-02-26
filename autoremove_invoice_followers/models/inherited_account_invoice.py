54a5aa4381e466eb408ee2bf4226c437399d9e53
GentecAustralia <46512820+GentecAustralia@users.noreply.github.com>
2019-02-26 19:40:07 +1100

    Update inherited_account_invoice.py

--- autoremove_invoice_followers/models/inherited_account_invoice.py
+++ autoremove_invoice_followers/models/inherited_account_invoice.py
@@ -3,20 +3,20 @@ from odoo import api, fields, models, _
 
 class MailFollowers(models.Model):
     _inherit = "mail.followers"
-
+    
     @api.model_create_multi
     def create(self, vals_list):
         new_vals_list = []
         for vals in vals_list:
             if vals.get('res_model') != 'account.invoice':
                 new_vals_list.append(vals)
+        
+    return super(MailFollowers, self).create(new_vals_list)
 
-        return super(Followers, self).create(new_vals_list)
 
-
-    @api.model
+@api.model
     def create(self, vals):
         if vals.get('res_model') != 'account.invoice':
-            return super(Followers, self).create(vals)
+            return super(MailFollowers, self).create(vals)
         else:
             return self.env['mail.followers']
