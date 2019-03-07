b31dc626e5d99b01a0d91000fb267c70bba0e5ad
GentecAustralia <46512820+GentecAustralia@users.noreply.github.com>
2019-03-08 09:26:15 +1100

    Add files via upload

---
+++ hv_delivery_display_bom/custom_model.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from odoo.exceptions import ValidationError
+from odoo import api, exceptions, fields, models, _
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    internal_reference = fields.Char(string='Internal Reference')
+
+    def write(self, vals):
+    	if len(self)==1 and self.state == 'assigned' and not vals.get('internal_reference'):
+        	for move in self.move_ids_without_package:
+	        	if move.product_tmpl_id.bom_count or not self.internal_reference:
+	        		self.internal_reference = move.product_tmpl_id.default_code
+    	return super(StockPicking, self).write(vals)
+
