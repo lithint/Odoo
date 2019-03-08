5a0c29fe01de19f93fa4f6ca3ae18e8e25331302
GentecAustralia <46512820+GentecAustralia@users.noreply.github.com>
2019-03-09 06:55:17 +1100

    Change Invoice date to todays date

--- domain366/custom_au_in/models/custom_stock_picking.py
+++ domain366/custom_au_in/models/custom_stock_picking.py
@@ -103,6 +103,7 @@ class StockPicking(models.Model):
             account = self.get_account_properties()
             account_invoice_obj.write({'user_id': sale_order.user_id.id})
             account_invoice_obj.write({'name': sale_order.client_order_ref})
+            account_invoice_obj.write(string='Your string', default=datetime.today())
             for inv_lines in account_invoice_obj.invoice_line_ids:
                 inv_lines.unlink()
             for sale_line in sale_order.order_line:
