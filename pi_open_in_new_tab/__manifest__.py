25ad69e6ea455ba21dfe5f80f2faf42624050c9b
pmlap131@gmail.com <pmlap131@gmail.com>
2019-04-03 09:53:12 +0700

    [ADD] module pi_open_in_new_tab

---
+++ pi_open_in_new_tab/__manifest__.py
@@ -0,0 +1,26 @@
+
+{
+    'name': 'PI Open Record In New Tab - 50% Newyear Off',
+    'summary': 'Open Record in  Tab - 50% Newyear Off',
+    'version': '12.0.0',
+    'description': """
+        This module will enable you to open any record in a new tab from the tree view. Refer index.html for detailed instructions.
+    """,
+    'author': 'Pierce Infotech',
+    'website': 'http://www.pierceinfotech.com',
+    "sequence": 1,
+    'price': 10,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+    'images': [
+        'images/main_screenshot.png','images/main_1.png','images/main_2.png',
+    ],
+    'depends': ['base','web'],
+    'data': [
+        'views/web_template.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
