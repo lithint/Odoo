c474935c2e302cb1f21383f391051cb8ecff0d58
Lap Dang <xuanlapd@gmail.com>
2019-04-09 10:26:58 +0700

    [MOD] show gst column

---
+++ gentec_custom/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Gentec - Custom',
+    'version': '1.1',
+    'category': 'Customization',
+    'description': """
+Gentec Customization Module
+    """,
+    'author': "Havi Technology",
+    'website': "havi.com.au",
+    'depends': ['sale', 'account'],
+    'data': [
+        'views/custom_sale_order.xml',
+        'views/custom_invoice.xml',
+    ],
+}
