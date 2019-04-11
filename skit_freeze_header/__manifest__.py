46961d34f7acb173c1260df7e238cb9c1d502d65
Lap Dang <xuanlapd@gmail.com>
2019-04-11 13:14:17 +0700

    [MOD] add module freeze header

---
+++ skit_freeze_header/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Freeze List View Header",
+
+    'summary': """
+        To freeze list view's & grid view's header, very helpful when dealing with many record.
+    """,
+
+    'description': """
+        To freeze list view's & grid view's header, very helpful when dealing with many record.
+    """,
+    'price': 25,
+    'currency': 'EUR',
+    'author': "Srikesh Infotech",
+    'license': "AGPL-3",
+    'website': "www.srikeshinfotech.com",
+    'category': 'web',
+    'version': '1.0',
+    'images': ['images/main_screenshot.png'],
+    'depends': ['web','base'],
+    'data': [
+        "views/freeze_head.xml",
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
