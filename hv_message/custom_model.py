9a20ede4956269d2fb12286c1c52458210910467
pmlap131@gmail.com <pmlap131@gmail.com>
2019-04-30 14:00:31 +0700

    [ADD] hv_message and hv_send_remittance_advice

---
+++ hv_message/custom_model.py
@@ -0,0 +1,265 @@
+# -*- coding: utf-8 -*-
+import os
+import base64
+import itertools
+import unicodedata
+import chardet
+import io
+import operator
+
+from itertools import groupby
+from odoo.exceptions import UserError, ValidationError
+from odoo import api, exceptions, fields, models, _
+from odoo.tools.mimetypes import guess_mimetype
+from odoo.tools import config, DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT, pycompat
+
+try:
+    import xlrd
+    try:
+        from xlrd import xlsx
+    except ImportError:
+        xlsx = None
+except ImportError:
+    xlrd = xlsx = None
+
+try:
+    from . import odf_ods_reader
+except ImportError:
+    odf_ods_reader = None
+
+FILE_TYPE_DICT = {
+    'text/csv': ('csv', True, None),
+    'application/vnd.ms-excel': ('xls', xlrd, 'xlrd'),
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': ('xlsx', xlsx, 'xlrd >= 1.0.0'),
+    'application/vnd.oasis.opendocument.spreadsheet': ('ods', odf_ods_reader, 'odfpy')
+}
+OPTIONS = {
+    'advanced': False, 
+    'bank_stmt_import': True, 'date_format': '', 'datetime_format': '', 'encoding': 'ascii', 'fields': [], 'float_decimal_separator': '.', 'float_thousand_separator': ',', 'headers': True, 'keep_matches': False, 'name_create_enabled_fields': {'currency_id': False, 'partner_id': False}, 'quoting': '"', 'separator': ','}
+
+module = os.path.dirname(__file__)
+module = module[module.rfind('/')+1:]
+
+
+class HaviDialog(models.TransientModel):
+    _name = 'havi.message'
+
+    module= fields.Char()
+    title = fields.Char('Title', readonly=True)
+    name = fields.Text('Name', readonly=True)
+    data_file = fields.Binary(string='Select file to import', help='Get your file to import and select them here.')
+    filename = fields.Char(string='File Name')
+
+    def get_module(self):
+        module = os.path.dirname(__file__)
+        return module[module.rfind('/')+1:]
+
+    @api.multi
+    def action_warning(self, message, title=None):
+        m = self.create({
+            'module': self.get_module(),
+            'title': title or 'Warning',
+            'name': message,
+        })
+        return {
+            'name': m.title,
+            'view_type': 'form',
+            'view_mode': 'form',
+            'view_id': self.env.ref('hv_message.warning').id,
+            'res_model': 'havi.message',
+            'type': 'ir.actions.act_window',
+            'res_id': m.id,
+            'target': 'new',
+        }
+
+    @api.multi
+    def action_confirm(self, message, title=None, module=None):
+        if not module or not title:
+            m = self.create({
+            'title': 'Warning',
+            'name': "To use Confirm dialog box, you must enter Title and Module values:\n\
+self.env['havi.message'].with_context(active_model='Your model',batch_invoice_id='Your model id').action_confirm('Message', 'Title', 'Your module name')",
+            })
+            return {
+                'name': m.title,
+                'view_type': 'form',
+                'view_mode': 'form',
+                'view_id': self.env.ref('hv_message.warning').id,
+                'res_model': 'havi.message',
+                'type': 'ir.actions.act_window',
+                'res_id': m.id,
+                'target': 'new',
+            }
+        m = self.create({
+            'module': module,
+            'title': title,
+            'name': message,
+        })
+        return {
+            'name': m.title,
+            'view_type': 'form',
+            'view_mode': 'form',
+            'view_id': self.env.ref('hv_message.confirm').id,
+            'res_model': 'havi.message',
+            'type': 'ir.actions.act_window',
+            'res_id': m.id,
+            'target': 'new',
+            'context': self._context,
+        }
+
+    @api.multi
+    def action_import(self, message, title=None, module=None):
+        if not module or not title:
+            m = self.create({
+            'title': 'Warning',
+            'name': "To use Import dialog box, you must enter Title and Module values:\n\
+self.env['havi.message'].with_context(active_model='Your model',batch_invoice_id='Your model id').action_import('Message', 'Title', 'Your module name')",
+            })
+            return {
+                'name': m.title,
+                'view_type': 'form',
+                'view_mode': 'form',
+                'view_id': self.env.ref('hv_message.warning').id,
+                'res_model': 'havi.message',
+                'type': 'ir.actions.act_window',
+                'res_id': m.id,
+                'target': 'new',
+            }
+        m = self.create({
+            'module': module,
+            'title': title,
+            'name': message,
+        })
+        return {
+            'name': m.title,
+            'view_type': 'form',
+            'view_mode': 'form',
+            'view_id': self.env.ref('hv_message.import').id,
+            'res_model': 'havi.message',
+            'type': 'ir.actions.act_window',
+            'res_id': m.id,
+            'target': 'new',
+            'context': self._context,
+        }
+
+    @api.multi
+    def action_confirm_yes(self):
+        m = self.create({
+            'title': 'Warning',
+            'name': "Copy this function to your code:\n\
+class hv_message(models.TransientModel):\n\
+    _name = 'havi.message'\n\
+    _inherit = 'havi.message'\n\
+\n\
+    def action_confirm_yes(self):\n\
+        if self.module=='%s' and self.title=='%s': \n\
+            Your action code here!!!\n\
+        else:\n\
+            return super(hv_message, self).action_confirm_yes()" % (self.module, self.title),
+        })
+        return {
+            'name': m.title,
+            'view_type': 'form',
+            'view_mode': 'form',
+            'view_id': self.env.ref('hv_message.warning').id,
+            'res_model': 'havi.message',
+            'type': 'ir.actions.act_window',
+            'res_id': m.id,
+            'target': 'new',
+        }
+
+    @api.multi
+    def import_file(self):
+        m = self.create({
+            'title': 'Warning',
+            'name': "Copy this function to your code:\n\
+class hv_message(models.TransientModel):\n\
+    _name = 'havi.message'\n\
+    _inherit = 'havi.message'\n\
+\n\
+    def import_file(self):\n\
+        if self.module=='%s' and self.title=='%s': \n\
+            Your action code here!!!\n\
+        else:\n\
+            return super(hv_message, self).import_file()" % (self.module, self.title),
+        })
+        return {
+            'name': m.title,
+            'view_type': 'form',
+            'view_mode': 'form',
+            'view_id': self.env.ref('hv_message.warning').id,
+            'res_model': 'havi.message',
+            'type': 'ir.actions.act_window',
+            'res_id': m.id,
+            'target': 'new',
+        }
+
+    def get_data(self):
+        if not self._check_csv(self.filename):
+            raise UserError(_('Cannot verify your .csv file.'))
+        csv_data = base64.b64decode(self.data_file) or b''
+        if not csv_data:
+            raise UserError(_('No data found in your .csv file.'))
+        rows = self._read_csv(csv_data, OPTIONS)
+        fields = list(itertools.islice(rows, 1))
+        if not fields:
+            raise UserError(_("You must configure any data in csv file."))
+        fields = fields[0]
+        indices = [index for index, field in enumerate(fields) if field]
+        if not indices:
+            raise UserError(_("You must configure any field in csv file."))
+        # If only one index, itemgetter will return an atom rather
+        # than a 1-tuple
+        if len(indices) == 1:
+            mapper = lambda row: [row[indices[0]]]
+        else:
+            mapper = operator.itemgetter(*indices)
+        datas = [
+            list(row) for row in pycompat.imap(mapper, rows)
+            if any(row)
+        ]  
+        return fields, datas
+
+    def _read_csv(self, csv_data, options):
+        """ Returns a CSV-parsed iterator of all non-empty lines in the file
+            :throws csv.Error: if an error is detected during CSV parsing
+        """
+        encoding = options.get('encoding')
+        if not encoding:
+            encoding = options['encoding'] = chardet.detect(csv_data)['encoding'].lower()
+
+        if encoding != 'utf-8':
+            csv_data = csv_data.decode(encoding).encode('utf-8')
+
+        separator = options.get('separator')
+        if not separator:
+            # default for unspecified separator so user gets a message about
+            # having to specify it
+            separator = ','
+            for candidate in (',', ';', '\t', ' ', '|', unicodedata.lookup('unit separator')):
+                # pass through the CSV and check if all rows are the same
+                # length & at least 2-wide assume it's the correct one
+                it = pycompat.csv_reader(io.BytesIO(csv_data), quotechar=options['quoting'], delimiter=candidate)
+                w = None
+                for row in it:
+                    width = len(row)
+                    if w is None:
+                        w = width
+                    if width == 1 or width != w:
+                        break # next candidate
+                else: # nobreak
+                    separator = options['separator'] = candidate
+                    break
+
+        csv_iterator = pycompat.csv_reader(
+            io.BytesIO(csv_data),
+            quotechar=options['quoting'],
+            delimiter=separator)
+
+        return (
+            row for row in csv_iterator
+            if any(x for x in row if x.strip())
+        )
+
+    def _check_csv(self, filename):
+        return filename and filename.lower().strip().endswith('.csv')
---
+++ hv_send_remittance_advice/custom_model.py
@@ -0,0 +1,221 @@
+# -*- coding: utf-8 -*-
+
+from odoo.exceptions import UserError, ValidationError
+from odoo import api, exceptions, fields, models, _
+from odoo.tools import config, DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT, pycompat
+
+
+MAP_INVOICE_TYPE_PARTNER_TYPE = {
+    'out_invoice': 'customer',
+    'out_refund': 'customer',
+    'in_invoice': 'supplier',
+    'in_refund': 'supplier',
+}
+MAP_INVOICE_TYPE_PAYMENT_SIGN = {
+    'out_invoice': 1,
+    'in_refund': -1,
+    'in_invoice': -1,
+    'out_refund': 1,
+}
+OPTIONS = {
+    'advanced': False, 
+    'bank_stmt_import': True, 'date_format': '', 'datetime_format': '', 'encoding': 'ascii', 'fields': [], 'float_decimal_separator': '.', 'float_thousand_separator': ',', 'headers': True, 'keep_matches': False, 'name_create_enabled_fields': {'currency_id': False, 'partner_id': False}, 'quoting': '"', 'separator': ','}
+
+class hv_account_abstract_payment(models.AbstractModel):
+    _inherit = "account.abstract.payment"  
+    
+
+class hv_account_register_payment(models.TransientModel):
+    _inherit = "account.register.payments"
+
+    @api.multi
+    def _prepare_payment_vals(self, invoices):
+        values = super(hv_account_register_payment, self)._prepare_payment_vals(invoices)
+        if self._context.get('batch_payment_id'):
+            values.update({
+                'batch_payment_id' : self._context.get('batch_payment_id'),
+            })
+        return values
+
+    @api.multi
+    def create_payments(self):
+        if self.payment_method_id.code == 'aba_ct':
+            batch_payment = self.env['account.batch.payment'].create({
+                'state' : 'draft',
+                'batch_type' : 'outbound',
+                'journal_id' : self.journal_id.id,
+                'payment_method_id' : self.payment_method_id.id,
+            })
+            self = self.with_context(batch_payment_id=batch_payment.id)
+
+        action_vals = super(hv_account_register_payment, self).create_payments()
+        
+        if self.payment_method_id.code == 'aba_ct':
+            action_vals = {
+            'name': _('Batch Payments'),
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'account.batch.payment',
+            'view_id': False,
+            'res_id': batch_payment.id,
+            'type': 'ir.actions.act_window',
+            'target': 'current',
+            }
+        
+        return action_vals
+
+class hv_account_payment(models.Model):
+    _inherit = "account.payment"
+
+    email_send = fields.Integer(string='Send Times', default=0, readonly=True)
+    email_vendor  = fields.Char(string='Email Address')
+    email_cc  = fields.Char(string='CC')
+    email_bcc  = fields.Char(string='BCC')
+    payment_reference = fields.Char(string='Payment Reference')
+
+    @api.model
+    def create(self, vals):
+        if self._context.get('batch_payment_id'):
+            vals.update({'email_vendor': self.env['res.partner'].browse(vals.get('partner_id')).email})
+        return super(hv_account_payment, self).create(vals)
+
+class hv_batch_email_send_abs(models.AbstractModel):
+    _name = "batch.payment.email.send.abstract"
+
+    name = fields.Char(string='Name')
+    currency_id = fields.Many2one('res.currency')
+    date = fields.Date()
+    partner_id = fields.Many2one('res.partner')
+    email_vendor  = fields.Char(string='Email Address')
+    email_cc  = fields.Char(string='CC')
+    payment_ids = fields.Many2many('account.payment')
+
+class hv_batch_email_send(models.TransientModel):
+    _name = "batch.payment.email.send"
+    _inherit = "batch.payment.email.send.abstract"
+
+    total = fields.Monetary(compute="_total")
+
+    @api.one
+    @api.depends('payment_ids')
+    def _total(self):
+        self.total=0
+        if self.payment_ids:
+            for item in self.payment_ids:
+                self.total += item.amount
+
+class hv_account_batch_payment(models.Model):
+    _inherit = 'account.batch.payment'
+
+    payment_ids = fields.One2many('account.payment', 'batch_payment_id', string="Payments", required=False,  readonly=True, states={'draft': [('readonly', False)]})
+
+    @api.constrains('batch_type', 'journal_id', 'payment_ids')
+    def _check_payments_constrains(self):
+        for record in self:
+            if record.payment_ids:
+                all_companies = set(record.payment_ids.mapped('company_id'))
+                if len(all_companies) > 1:
+                    raise ValidationError(_("All payments in the batch must belong to the same company."))
+                all_journals = set(record.payment_ids.mapped('journal_id'))
+                if len(all_journals) > 1 or record.payment_ids[0].journal_id != record.journal_id:
+                    raise ValidationError(_("The journal of the batch payment and of the payments it contains must be the same."))
+                all_types = set(record.payment_ids.mapped('payment_type'))
+                if len(all_types) > 1:
+                    raise ValidationError(_("All payments in the batch must share the same type."))
+                if all_types and record.batch_type not in all_types:
+                    raise ValidationError(_("The batch must have the same type as the payments it contains."))
+                all_payment_methods = set(record.payment_ids.mapped('payment_method_id'))
+                if len(all_payment_methods) > 1:
+                    raise ValidationError(_("All payments in the batch must share the same payment method."))
+                if all_payment_methods and record.payment_method_id not in all_payment_methods:
+                    raise ValidationError(_("The batch must have the same payment method as the payments it contains."))
+    
+    def validate_batch(self):
+        records = self.filtered(lambda x: x.state == 'draft')
+        for rec in records:
+            if rec.payment_ids:
+                i = 1
+                group_partner = []
+                for pm in rec.payment_ids:
+                    if pm.partner_id.id not in group_partner:
+                        group_partner.append(pm.partner_id.id)
+                
+                for g in group_partner:
+                    for pm in rec.payment_ids:
+                        if pm.partner_id.id == g:
+                            pm.write({'state':'sent', 'payment_reference' : 'BO' + rec.name[-10:] + '/' + str(i)})
+                    i += 1  
+
+        records.write({'state': 'sent'})
+        records = self.filtered(lambda x: x.file_generation_enabled)
+        if records:
+            return self.export_batch_payment()
+
+    @api.model
+    def create(self, vals):
+        rec = super(hv_account_batch_payment, self).create(vals)
+        if rec.payment_ids:
+            for pm in rec.payment_ids:
+                if not pm.email_vendor:
+                    pm.write({'email_vendor' : pm.partner_id.email})    
+        return rec
+
+    @api.multi
+    def write(self, vals):
+        rec = super(hv_account_batch_payment, self).write(vals)
+        if 'payment_ids' in vals:
+            rec = self
+            for pm in rec.payment_ids:
+                if not pm.email_vendor:
+                    pm.write({'email_vendor' : pm.partner_id.email})          
+        return rec
+
+    def action_send_remittance_advice(self):
+        vendor = {}
+        for item in self.payment_ids:
+            if not vendor.get(item.partner_id.id):
+                vendor.update({item.partner_id.id:{
+                    'partner_id': item.partner_id.id,
+                    'email_vendor': item.email_vendor,
+                    'email_cc': item.email_cc,
+                    'payment_ids':[item.id]
+                }})
+            else:
+                if not vendor[item.partner_id.id]['email_vendor']:
+                    vendor[item.partner_id.id]['email_vendor'] = item.email_vendor
+                if not vendor[item.partner_id.id]['email_cc']:
+                    vendor[item.partner_id.id]['emal_cc'] = item.email_cc
+                vendor[item.partner_id.id]['payment_ids'].append(item.id) 
+        if not vendor:
+            return self.env['havi.message'].action_warning('Payment to send not found.','Send Remittance Advice')
+
+        default_template = self.env.ref('hv_send_remittance_advice.email_template_batch_payment', False)      
+        for item in vendor:
+            batch = self.env['batch.payment.email.send'].create({
+                'name': self.name,
+                'date': self.date,
+                'currency_id': self.currency_id.id,
+                'partner_id': vendor[item]['partner_id'],
+                'email_vendor': vendor[item]['email_vendor'],
+                'email_cc': vendor[item]['email_cc'],
+                'payment_ids':[(6, 0, vendor[item]['payment_ids'])]
+            })
+            # batch._cr.commit()
+            ctx = dict(
+                active_model='batch.payment.email.send',
+                active_id=batch.id,
+                default_model='batch.payment.email.send',
+                default_res_id=batch.id,
+                default_use_template=bool(default_template),
+                default_template_id=default_template and default_template.id or False,
+                # default_composition_mode='comment',
+                default_composition_mode='mass_mail',
+
+            )
+            wizard_mail = self.env['mail.compose.message'].with_context(ctx).create({})
+            wizard_mail.onchange_template_id_wrapper()
+            wizard_mail.send_mail()
+            for pid in batch.payment_ids:
+                pid.email_send += 1
+        return self.env['havi.message'].action_warning('Send remittance advice completed.','Send Remittance Advice')
+
