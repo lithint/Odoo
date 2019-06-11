7d516233a3020b5d801c733658aa1fa434b45684
pmlap131@gmail.com <pmlap131@gmail.com>
2019-06-11 17:38:23 +0700

    [ADD] Finished product - Potential Qty [ADD] Product - Qty per warehouse

--- gentec_custom/__manifest__.py
+++ gentec_custom/__manifest__.py
@@ -1,17 +1,19 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# -*- encoding: utf-8 -*-
 {
-    'name': 'Gentec - Custom',
-    'version': '1.1',
-    'category': 'Customization',
-    'description': """
-Gentec Customization Module
-    """,
+    'name': 'Gentec Custom',
+    'version': '1.0.1',
+    'category': 'Gentec Custom',
+    'summary': 'Gentec Custom',
+    'description': "",
     'author': "Havi Technology",
     'website': "havi.com.au",
-    'depends': ['sale', 'account'],
-    'data': [
-        'views/custom_sale_order.xml',
-        'views/custom_invoice.xml',
+    'depends': [
+        'mrp', 'sale',
     ],
+    'data': [
+        'views/product_views.xml',
+        'views/sale_order_views.xml',
+    ],
+    'demo_xml': [],
+    'installable': True,
 }
--- gentec_custom/models/sale_order.py
+++ gentec_custom/models/sale_order.py
@@ -1,21 +1,8 @@
 # -*- coding: utf-8 -*-
-from odoo.exceptions import UserError, ValidationError
-from odoo import api, exceptions, fields, models, _
+import math
+from odoo import api, fields, models
 
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
 
-    @api.model
-    def _default_warehouse_id(self):
-        result = super(SaleOrder, self)._default_warehouse_id()
-        return []
-
-    x_studio_foc = fields.Selection([('Sample', 'Sample'),
-                                     ('Marketing', 'Marketing'),
-                                     ('After Sales & Services', 'After Sales & Services'),
-                                     ('Special Arrangement', 'Special Arrangement'),
-                                     ('Swap Product', 'Swap Product'),
-                                     ('Kit items', 'Kit items'),
-                                     ('Not Applicable', 'Not Applicable'),], string='FOC', default='Not Applicable', required=True)
-    warehouse_id = fields.Many2one('stock.warehouse', string='Warehouse',required=True, readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
-        default=_default_warehouse_id)
+    qty_per_warehouse = fields.Text(related='product_id.qty_per_warehouse' ,string='Available Quantity', readonly=True)
