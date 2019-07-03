8d05030f9a6d010353f4f7d9cc3860536bfea994
pmlap131@gmail.com <pmlap131@gmail.com>
2019-07-03 16:12:14 +0700

    [MOD] Kit Potential qty per warehouse

--- gentec_custom/models/sale_order.py
+++ gentec_custom/models/sale_order.py
@@ -6,3 +6,4 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     x_studio_available_qty = fields.Text(related='product_id.x_studio_quantity' ,string='Available Quantity', readonly=True)
+    potential_qty = fields.Text(related='product_id.potential_qty' ,string='Potential Quantity', readonly=True)
