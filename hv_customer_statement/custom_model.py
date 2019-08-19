302ce4daef21ff8b3c20c36521c9ef3edebb2210
pmlap131@gmail.com <pmlap131@gmail.com>
2019-08-19 11:54:57 +0700

    [MOD] Customer statement cut off date is not right

--- hv_customer_statement/custom_model.py
+++ hv_customer_statement/custom_model.py
@@ -138,11 +138,11 @@ class hv_customer_statement_line(models.Model):
             query = """
                     SELECT max(m.id), max(m.id), max(m.id)
                     from account_move_line m 
-                        inner join account_invoice i on m.invoice_id=i.id
+                        inner join account_invoice i on m.invoice_id=i.id and i.amount_total_signed!=0 and i.state='open'
 	                    inner join account_account a on m.account_id = a.id 
 		                    and a.deprecated=false and a.internal_type ='receivable'
                     where m.reconciled=false and m.blocked=false
-                        and m.date >= '%s' and m.date <= '%s'
+                        and m.date AT TIME ZONE 'UTC' >= '%s' and m.date AT TIME ZONE 'UTC' <= '%s'
                         and (i.partner_id = %s) 
                         and m.invoice_id is not null 
                         group by m.invoice_id
@@ -152,7 +152,7 @@ class hv_customer_statement_line(models.Model):
 	                    inner join account_account a on m.account_id = a.id 
 		                    and a.deprecated=false and a.internal_type ='receivable'
                     where m.reconciled=false and m.blocked=false
-                        and m.date >= '%s' and m.date <= '%s'
+                        and m.date AT TIME ZONE 'UTC' >= '%s' and m.date AT TIME ZONE 'UTC' <= '%s'
                         and (m.partner_id = %s) 
                         and m.invoice_id is null 
                     """ % (start_date, statement_date + timedelta(days=1), self.customer_id.id, start_date, statement_date + timedelta(days=1), self.customer_id.id)
@@ -177,10 +177,11 @@ class hv_customer_statement_line(models.Model):
             query = """
                     SELECT max(m.id), max(m.id), max(m.id)
                     from account_move_line m 
+                        inner join account_invoice i on m.invoice_id=i.id and i.amount_total_signed!=0 and i.state='open'
 	                    inner join account_account a on m.account_id = a.id 
 		                    and a.deprecated=false and a.internal_type ='receivable'
                     where m.reconciled=false and m.blocked=false
-                        and m.date >= '%s' and m.date <= '%s'
+                        and m.date AT TIME ZONE 'UTC' >= '%s' and m.date AT TIME ZONE 'UTC' <= '%s'
                         and (m.partner_id = %s)
                         and m.invoice_id is not null 
                         group by m.invoice_id
@@ -190,7 +191,7 @@ class hv_customer_statement_line(models.Model):
 	                    inner join account_account a on m.account_id = a.id 
 		                    and a.deprecated=false and a.internal_type ='receivable'
                     where m.reconciled=false and m.blocked=false
-                    and m.date >= '%s' and m.date <= '%s'
+                    and m.date AT TIME ZONE 'UTC' >= '%s' and m.date AT TIME ZONE 'UTC' <= '%s'
                         and (m.partner_id = %s)
                         and m.invoice_id is null 
                     """ % (start_date, statement_date + timedelta(days=1), self.customer_id.id, start_date, statement_date + timedelta(days=1), self.customer_id.id)
@@ -281,7 +282,7 @@ class hv_customer_statement(models.Model):
                             inner join account_account a on m.account_id = a.id 
                                 and a.deprecated=false and a.internal_type ='receivable'
                         where m.reconciled=false and m.blocked=false
-                            and m.date >= '%s' and m.date <= '%s')
+                            and m.date AT TIME ZONE 'UTC' >= '%s' and m.date AT TIME ZONE 'UTC' <= '%s')
                     """ % (start_date, statement_date + timedelta(days=1))
             partner_ids = self.env['account.invoice']._search_id(query)
             lself = self.env['hv.customer.statement.line'].search([('statement_id', '=', self.id), ('consolidatedsm', '=', True)])
